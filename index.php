cefde04d2e02e47949c803e2cd269465f43f9a67
avanootegem <aurelien.vanootegem@gmail.com>
2020-12-09 15:51:40 +0100

    Debut Formulaire

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include_once("header.php"); ?>
+
+<section id="form">
+    <form method="post">
+        <input type="text" name="mail" id="mail" placeholder="Adresse mail destinataire" class="inputText">
+        <input type="text" name="destinataire" id="destinataire" placeholder="Prénom du destinataire" class="inputText">
+        <input type="text" name="envoyeur" id="envoyeur" placeholder="Prénom de l'envoyeur" class="inputText">
+    </form>
+</section>
+
+<?php include_once("footer.php"); ?>
