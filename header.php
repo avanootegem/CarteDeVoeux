b7feccf65550ae6bd92a989fdfa98e9ba461d46d
avanootegem <aurelien.vanootegem@gmail.com>
2020-12-15 15:35:31 +0100

    Favicon ajoutée

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Aurélien Vanootegem </title>
+    <title> Carte de Voeux </title>
     <link rel="stylesheet" href="assets/scss/reset.css">
     <link rel="stylesheet" href="assets/scss/fonts.css">
     <link rel="stylesheet" href="assets/scss/main.css">
